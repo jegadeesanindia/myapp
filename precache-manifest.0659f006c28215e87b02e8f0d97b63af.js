3ebca900fc408415a7ae6ffd6ddcd1447e5b79c0
jegadeesanindia <jegadeesanayr@gmail.com>
2020-05-31 00:14:40 +0530

    Updates

---
+++ precache-manifest.0659f006c28215e87b02e8f0d97b63af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3061c15214c2ede58128002f4f839fb5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b254f187c57178a2dfb0",
+    "url": "/myapp/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "a7564bb9f09a28eca0c9",
+    "url": "/myapp/static/js/2.81db8de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.81db8de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b254f187c57178a2dfb0",
+    "url": "/myapp/static/js/main.f8b5f125.chunk.js"
+  },
+  {
+    "revision": "0f52625f54f70bbb71e5",
+    "url": "/myapp/static/js/runtime-main.a2ba8911.js"
+  }
+]);
